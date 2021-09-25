6a15506b658bf0c04118d7aaa9c8a135f2266e2a
giovane1-8 <76892115+giovane1-8@users.noreply.github.com>
2021-09-25 17:35:54 -0300

    Update Model.php

--- models/Model.php
+++ models/Model.php
@@ -4,10 +4,10 @@
     class Model{
         protected object $PDO;
         function __construct(){
-            $username = "giovane589";
-            $password = "privada123123";
+            $username = "nwdlseoncbxaab";
+            $password = "e288fe6e576699d91e52bc300c2ebce092edd89fab3b33f526c765904cd866ee";
             try {
-                $this -> PDO = new \PDO('mysql:host=db4free.net:3306;dbname=mangaskindle', $username, $password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                $this -> PDO = new \PDO('mysql:host=ec2-3-233-100-43.compute-1.amazonaws.com:5432;dbname=dd741ac9mgmg6b', $username, $password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
                 $this -> PDO -> setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             } catch ( \PDOException $e) {
                 echo 'ERROR: ' . $e->getMessage();
