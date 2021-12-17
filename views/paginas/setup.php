20a5bd17877dabdb0c94f8caba2f3d31da4a60fc
giovane1-8 <giovane.gi2012@hotmail.com>
2021-12-17 01:09:42 -0300

    formulario de manga

--- views/paginas/setup.php
+++ views/paginas/setup.php
@@ -1,1 +1,32 @@
-<h1>Pagina do administrador</h1>
+<h1>Adicionar manga</h1>
+<form>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Nome</span>
+        </div>
+        <input type="text" placeholder="jujutsu kaisen" class="form-control" name="nome_manga" required>
+    </div>
+    
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Editora</span>
+        </div>
+        <input type="text" placeholder="Panini" class="form-control" name="editora" required>
+    </div>
+
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Sinopse</span>
+        </div>
+        <input type="text" placeholder="Apesar do estudante colegial Yuuji Itadori ter grande força física, ele se inscreve no Clube de Ocultismo. Certo dia, eles encontram um “objeto amaldiçoado” e retiram o selo, atraindo criaturas chamadas de “maldições”. Itadori corre em socorro de seus colegas, mas será que ele será capaz de abater essas criaturas usando apenas a força física?!" class="form-control" name="sinopse" required>
+    </div>
+    
+    <div class="input-group">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Sinopse</span>
+        </div>
+        <input type="text" placeholder="Apesar do estudante colegial Yuuji Itadori ter grande força física, ele se inscreve no Clube de Ocultismo. Certo dia, eles encontram um “objeto amaldiçoado” e retiram o selo, atraindo criaturas chamadas de “maldições”. Itadori corre em socorro de seus colegas, mas será que ele será capaz de abater essas criaturas usando apenas a força física?!" class="form-control" name="sinopse" required>
+    </div>
+</form>
+
+<h1>Adicionar capitulo de manga</h1>
